6e511ffec214a659cf8586d7fcf631d3a44f6c6d
ram-pc <kaucha.ram@gmail.com>
2024-08-27 21:37:09 +1000

    Quartz sync: Aug 27, 2024, 9:37 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "note.ramkaucha.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
